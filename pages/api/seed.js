7bbad381db6bbff99a1b98483f938332f62ea3de
Kubilay Bozak <48950686+kubilaybzk@users.noreply.github.com>
2022-08-17 14:00:24 +0300

    Ufak biz düzeltme yaptık.
    Ufak biz düzeltme yaptık.

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,14 +1,11 @@
-import User from "../../Models/User";
-import data from "../../utils/data";
-import db from "../../utils/db";
-import product from "../../Models/Products";
+import User from '../../models/User';
+import data from '../../utils/data';
+import db from '../../utils/db';
 
 const handler = async (req, res) => {
   await db.connect();
   await User.deleteMany();
   await User.insertMany(data.users);
-  await product.deleteMany();
-  await product.insertMany(data.products);
   await db.disconnect();
   res.send({ message: 'seeded successfully' });
 };
